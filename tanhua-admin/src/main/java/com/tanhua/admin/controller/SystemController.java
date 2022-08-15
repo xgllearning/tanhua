5c454260d464a7593155f07ba1100b237bb67e05
xgl <34029067@qq.com>
2022-08-16 01:08:36 +0800

    第七十五次提交：探花交友APP建立的后台管理系统，目的是完成探花交友项目的业务闭环，主要功能包括：用户管理、动态管理、审核管理以及系统管理。（课程中实现的功能有：登录、首页、用户管理、动态审核。）  环境搭建  1、在navicat中导入tanhua-admin数据库  2、前端页面已经打包到nginx中，将资料中的tanhua-nginx解压到没有中文和特殊符号的目录  解压目录下tanhua-nginx/conf/nginx.conf已经指定了配置内容  浏览器访问：http://localhost:8088/ 3、IDEA中导入基础的模块代码

---
+++ tanhua-admin/src/main/java/com/tanhua/admin/controller/SystemController.java
@@ -0,0 +1,28 @@
+package com.tanhua.admin.controller;
+
+import cn.hutool.captcha.CaptchaUtil;
+import cn.hutool.captcha.LineCaptcha;
+import com.tanhua.admin.interceptor.AdminHolder;
+import com.tanhua.admin.service.AdminService;
+import com.tanhua.commons.utils.Constants;
+import com.tanhua.model.domain.Admin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/system/users")
+public class SystemController {
+
+    @Autowired
+    private AdminService adminService;
+
+    @Autowired
+    private RedisTemplate<String,String> redisTemplate;
+
+}
